358df8df87ff333440050bd22f285e3ef1778c93
cristsr <styven21121@gmail.com>
2022-03-15 20:52:47 -0500

    feat: initial commit

--- apps/stream-app/src/app/app.controller.ts
+++ apps/stream-app/src/app/app.controller.ts
@@ -1,13 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
-
+import { Controller, Get, Inject } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
 import { AppService } from './app.service';
+import { Observable } from 'rxjs';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    @Inject('AUTH_SERVICE') private client: ClientProxy
+  ) {}
 
   @Get()
   getData() {
     return this.appService.getData();
+  }
+
+  @Get('accumulate')
+  accumulate(): Observable<number> {
+    const pattern = { cmd: 'sum' };
+    const payload = [1, 2, 3, 6];
+    return this.client.send<number>(pattern, payload);
   }
 }
--- apps/stream-app/src/app/app.module.ts
+++ apps/stream-app/src/app/app.module.ts
@@ -2,9 +2,20 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'AUTH_SERVICE',
+        options: {
+          transport: Transport.TCP,
+          port: 6000,
+        },
+      },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
